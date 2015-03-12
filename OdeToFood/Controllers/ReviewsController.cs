30821b372084e0f249de35bc2353dd9bab4f41b2
Lydia Kwag <wag0325@gmail.com>
2015-03-12 18:53:07 -0400

    First commit - chapter 3 Razor Views

---
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -0,0 +1,142 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        //
+        // GET: /Reviews/
+        public ActionResult Index()
+        {
+            var model =
+                from r in _reviews
+                orderby r.Country
+                select r;
+
+            return View(model);
+        }
+
+        //
+        // GET: /Reviews/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Reviews/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Reviews/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Reviews/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Reviews/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Reviews/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Reviews/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        static List<RestaurantReview> _reviews = new List<RestaurantReview>
+        {
+            new RestaurantReview {
+                Id = 1,
+                Name = "Cinnamon Shop",
+                City = "Fairfax",
+                Country = "USA",
+                Rating = 10
+            },
+            new RestaurantReview {
+                Id = 1,
+                Name = "Cocoa Shop",
+                City = "Fairfax",
+                Country = "USA",
+                Rating = 10
+            },
+            new RestaurantReview {
+                Id = 1,
+                Name = "Sherbet Shop",
+                City = "State College",
+                Country = "USA",
+                Rating = 10
+            },
+            new RestaurantReview {
+                Id = 1,
+                Name = "Happy Shop",
+                City = "Beijing",
+                Country = "China",
+                Rating = 10
+            },
+            new RestaurantReview {
+                Id = 1,
+                Name = "Watermelon Shop",
+                City = "D.C.",
+                Country = "USA",
+                Rating = 10
+            }
+        };
+    }
+}
